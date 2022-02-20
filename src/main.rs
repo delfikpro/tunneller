35d0de0adcb3fd38d8b5d44f9d2fa79a363ffa0e
delfikpro <33707322+DelfikPro@users.noreply.github.com>
2022-02-20 06:37:20 +0300

    Temporarily hardcode nats dns name

--- src/main.rs
+++ src/main.rs
@@ -322,7 +322,7 @@ async fn main0() -> std::io::Result<()> {
 
 	let port_manager = Arc::new(create_port_manager(port_range_start, port_range_size));
 
-	let broker = Arc::new(nats::connect("127.0.0.1:4222")?);
+	let broker = Arc::new(nats::connect("nats-service:4222")?);
 
 	let broker_sub = broker.clone();
 
